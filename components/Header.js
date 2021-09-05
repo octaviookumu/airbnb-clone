933e2709cf58bfb17d35f7845c6ddcb900f375c0
Octavian Okumu <octaviookumu@gmail.com>
2021-09-05 18:05:16 +0300

    Set up calendar using react date range

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,34 @@ import {
   SearchIcon,
   UsersIcon
 } from "@heroicons/react/solid";
+import { useState } from 'react';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection'
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  const resetInput = () => {
+    setSearchInput('');
+  }
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
       {/* left */}
@@ -23,6 +49,8 @@ function Header() {
       {/* middle */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400 w-[180px]"
           type="text"
           name=""
@@ -34,7 +62,9 @@ function Header() {
 
       {/* right */}
       <div className="flex items-center justify-end space-x-4 text-gray-500">
-        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <p className="hidden md:inline cursor-pointer text-xs lg:text-lg">
+          Become a host
+        </p>
         <GlobeAltIcon className="hidden md:inline h-6 cursor-pointer" />
 
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
@@ -42,6 +72,34 @@ function Header() {
           <UserCircleIcon className="h-4 md:h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5861"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              min={1}
+              className="w-12 pl-2 outline-none text-red-400"
+            />
+          </div>
+          <div className="flex justify-around">
+            <button onClick={resetInput} className="text-gray-500">Cancel</button>
+            <button className="text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import SmallCard from "../components/SmallCard";
 
 export default function Home({ exploreData, cardsData }) {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
@@ -38,7 +38,7 @@ export default function Home({ exploreData, cardsData }) {
         </section>
 
         <section>
-          <h2 class="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardsData?.map(({ img, title }) => (
